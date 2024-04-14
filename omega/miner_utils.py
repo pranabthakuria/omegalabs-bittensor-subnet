50525f8705ea220ccb6a181c84fc003b064b8ab8
Pranab Thakuria <pranabthakuria@AS-MAC-0078.local>
2024-04-14 23:50:53 +0530

    code changes

--- omega/miner_utils.py
+++ omega/miner_utils.py
@@ -8,7 +8,10 @@ from omega.protocol import VideoMetadata
 from omega.imagebind_wrapper import ImageBind
 from omega.constants import MAX_VIDEO_LENGTH, FIVE_MINUTES
 from omega import video_utils
+import concurrent.futures
+import time
 
+from omega.video_utils import YoutubeResult
 
 if os.getenv("OPENAI_API_KEY"):
     from openai import OpenAI
@@ -43,7 +46,7 @@ def get_relevant_timestamps(query: str, yt: video_utils.YoutubeDL, video_path: s
     return start_time, end_time
 
 
-def search_and_embed_videos(query: str, num_videos: int, imagebind: ImageBind) -> List[VideoMetadata]:
+def process_result(result: YoutubeResult, query: str, imagebind: ImageBind, video_metas : List) -> VideoMetadata:
     """
     Search YouTube for videos matching the given query and return a list of VideoMetadata objects.
 
@@ -55,44 +58,63 @@ def search_and_embed_videos(query: str, num_videos: int, imagebind: ImageBind) -
         List[VideoMetadata]: A list of VideoMetadata objects representing the search results.
     """
     # fetch more videos than we need
-    results = video_utils.search_videos(query, max_results=int(num_videos))
-    video_metas = []
+
     try:
         # take the first N that we need
-        for result in results:
-            start = time.time()
-            download_path = video_utils.download_video(
-                result.video_id,
-                start=0,
-                end=min(result.length, FIVE_MINUTES)  # download the first 5 minutes at most
-            )
-            if download_path:
-                clip_path = None
-                try:
-                    result.length = video_utils.get_video_duration(download_path.name)  # correct the length
-                    print(f"Downloaded video {result.video_id} ({min(result.length, FIVE_MINUTES)}) in {time.time() - start} seconds")
-                    start, end = get_relevant_timestamps(query, result, download_path)
-                    description = get_description(result, download_path)
-                    clip_path = video_utils.clip_video(download_path.name, start, end)
-                    embeddings = imagebind.embed([description], [clip_path])
-                    video_metas.append(VideoMetadata(
-                        video_id=result.video_id,
-                        description=description,
-                        views=result.views,
-                        start_time=start,
-                        end_time=end,
-                        video_emb=embeddings.video[0].tolist(),
-                        audio_emb=embeddings.audio[0].tolist(),
-                        description_emb=embeddings.description[0].tolist(),
-                    ))
-                finally:
-                    download_path.close()
-                    if clip_path:
-                        clip_path.close()
-            if len(video_metas) == num_videos:
-                break
-
+        start = time.time()
+        download_path = video_utils.download_video(
+            result.video_id,
+            start=0,
+            end=min(result.length, FIVE_MINUTES)  # download the first 5 minutes at most
+        )
+        video_meta = None
+        if download_path:
+            clip_path = None
+            try:
+                result.length = video_utils.get_video_duration(download_path.name)  # correct the length
+                print(
+                    f"Downloaded video {result.video_id} ({min(result.length, FIVE_MINUTES)}) in {time.time() - start} seconds")
+                start, end = get_relevant_timestamps(query, result, download_path)
+                description = get_description(result, download_path)
+                clip_path = video_utils.clip_video(download_path.name, start, end)
+                embeddings = imagebind.embed([description], [clip_path])
+                video_meta = VideoMetadata(
+                    video_id=result.video_id,
+                    description=description,
+                    views=result.views,
+                    start_time=start,
+                    end_time=end,
+                    video_emb=embeddings.video[0].tolist(),
+                    audio_emb=embeddings.audio[0].tolist(),
+                    description_emb=embeddings.description[0].tolist(),
+                )
+            finally:
+                download_path.close()
+                if clip_path:
+                    clip_path.close()
     except Exception as e:
         bt.logging.error(f"Error searching for videos: {e}")
 
+    return video_meta
+
+
+
+def search_and_embed_videos(query: str, num_videos: int, imagebind: ImageBind) -> List[VideoMetadata]:
+
+    results = video_utils.search_videos(query, max_results=int(num_videos))
+    video_metas = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_videos) as executor:
+        # Submit each result for processing
+        futures = [executor.submit(process_result, result, query, imagebind, video_metas) for result in results]
+
+        # Wait for all tasks to complete
+        for future in concurrent.futures.as_completed(futures):
+            # Handle any exceptions raised by the task
+            try:
+                video_metas.extend(future.result())
+            except Exception as e:
+                bt.logging.error(f"Error processing result: {e}")
     return video_metas
+
+
+
